80e44edb2c9c71da42b221e7d26b6c378b661fa6
Michael Svoren <msvoren@ku.edu>
2019-03-25 09:32:40 -0500

    mySQL t/f works

--- CreatePosts.php
+++ CreatePosts.php
@@ -10,12 +10,29 @@ if ($mysqli->connect_errno)
     exit();
 }
 
-$checkExistence = "SELECT userID from Users WHERE userID = '$userID'";
+$checkExistence = "SELECT userID from Users where userID='$userID'";
 $insert = "INSERT INTO Posts (content, author_id) VALUES ('$message', '$userID');";
 
-$result = $mysqli->query($checkExistence);
+//echo "$checkExistence";
+//$result = $mysqli->query($checkExistence);
+//echo ($checkExistence);
+//$arr = array($result);
 
-if ($message == "" || $result == NULL)
+if ($result = $mysqli->query($checkExistence)) {
+
+    /* fetch associative array */
+    // while ($row = $result->fetch_assoc()) {
+    //     echo json_encode($row);
+    // }
+
+    /* free result set */
+  //  $result->free();
+}
+
+//echo mysql_num_rows($result);
+$arr = ($result->fetch_all());
+
+if ($message == "" || empty($arr))
 {
   echo "Unable to create post";
 }
