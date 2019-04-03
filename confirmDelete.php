e004f79ce04470aeb569d1b76809c4abe818f26e
Michael Svoren <msvoren@ku.edu>
2019-04-03 14:43:52 -0500

    post works

---
+++ confirmDelete.php
@@ -0,0 +1,26 @@
+<?php
+echo "confirmDelete file<br>";
+
+
+
+if (!empty($_POST['1']))
+{
+  echo "true";
+}
+else
+{
+  echo "false";
+}
+//echo "$check_value";
+
+/*
+if (isset($_POST['1']))
+{
+  echo "checked";
+}
+else
+{
+  echo "not checked";
+}*/
+
+?>
