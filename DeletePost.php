1714ba72fcc6daad78c5a9da8abb8a2c44cf52dc
Michael Svoren <msvoren@ku.edu>
2019-04-03 21:58:08 -0500

    functional delete

--- DeletePost.php
+++ DeletePost.php
@@ -1,26 +1,28 @@
 <?php
-
-
-    echo "Now in PHP";
-
     function generateList()
     {
       $mysqli = new mysqli("mysql.eecs.ku.edu", "m326s072", "xah7Aedi", "m326s072");
       $userID = $_POST['user'];
       //echo "$userID<br>";
       $sql = mysqli_query($mysqli, "SELECT * from Posts");
-      //echo "<table>";
       $checkIndex = 0;
+
+      echo "<tr>";
+      echo "<th>Content</th><th>User</th><th> Delete? </th>";
+      echo "</tr>";
+
       while ($row = $sql->fetch_assoc())
       {
         $postID = $row['post_id'];
         echo "<tr>";
-        echo "<td>" .$row['content'] . "</td><td>" . $row['author_id']. "</td><td> <input type=\"checkbox\" name=\"$postID\"></td>";
+        echo "<td>" .$row['content'] . "</td><td>" . $row['author_id']. "</td><td> <input type=\"checkbox\" name=\"$checkIndex\"></td>";
         echo "</tr>";
 
         $checkIndex++;
       }
-    //  echo "</table>";
+      $numPosts = $checkIndex;
+      echo "<input type=\"number\" name=\"numberOfPosts\" value=\"$numPosts\" style=\"display: none\">";
+
     }
 
 ?>
--- confirmDelete.php
+++ confirmDelete.php
@@ -1,26 +1,48 @@
 <?php
-echo "confirmDelete file<br>";
+echo "<h1>Delete Confirmation</h1><br>";
+
+$numPosts = ($_POST['numberOfPosts']);
 
 
-
-if (!empty($_POST['1']))
+//get an array of the post numbers that are to be deleted
+$deletables = array();
+for ($i = 0; $i < $numPosts; $i++)
 {
-  echo "true";
+  if (!empty($_POST[$i]))
+  {
+    $deletables[count($deletables)] = $i;
+  }
 }
-else
-{
-  echo "false";
-}
-//echo "$check_value";
 
-/*
-if (isset($_POST['1']))
-{
-  echo "checked";
-}
-else
-{
-  echo "not checked";
-}*/
+
+  $mysqli = new mysqli("mysql.eecs.ku.edu", "m326s072", "xah7Aedi", "m326s072");
+  //echo "$userID<br>";
+  $sql = mysqli_query($mysqli, "SELECT * from Posts");
+
+  //find the post id for all items to be deleted
+  $toDelete = array();
+  $currentIteration = 0;
+  while ($row = $sql->fetch_assoc())
+  {
+    for ($i = 0; $i < count($deletables); $i++)
+    {
+      if ($currentIteration == $deletables[$i])
+      {
+        $toDelete[count($toDelete)] = ($row['post_id']);
+      }
+    }
+    $currentIteration++;
+  }
+
+  //Delete all specified
+  for ($i = 0; $i<count($toDelete); $i++)
+  {
+    $num = $toDelete[$i];
+    echo "Deleted post_id: $num<br>";
+    $delete = "DELETE FROM Posts WHERE post_id = '$num';";
+    $mysqli->query($delete);
+  }
+
+
 
 ?>
